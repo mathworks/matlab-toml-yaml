2e1227f3db8d95c5f3dbf33dbbe6222f84044b87
Aylin Dmello <admello@mathworks.com>
2026-09-01 17:04:39 -0400

    Mex workaround for linux

--- buildfile.m
+++ buildfile.m
@@ -92,21 +92,6 @@ function testTask(~)
     assertSuccess(results);
 end
 
-function flags = staticLibcxxFlags()
-    % On Linux, statically link libstdc++ so the MEX binary works even when
-    % the system compiler is newer than MATLAB's bundled C++ runtime.
-    flags = string.empty;
-    if ~isunix || ismac
-        return
-    end
-    archDir = fullfile(matlabroot, "bin", computer("arch"));
-    extDir  = fullfile(matlabroot, "extern", "bin", computer("arch"));
-    flags = ["LDFLAGS=$LDFLAGS -static-libstdc++", ...
-        "LINKLIBS=-Wl,--as-needed" ...
-            + " -L" + archDir + " -L" + extDir ...
-            + " -lMatlabDataArray -lmx -lmex -lm -lmat"];
-end
-
 function mltbxTask(~)
     % Package toolbox/ into a .mltbx artifact.
     opts = matlab.addons.toolbox.ToolboxOptions("toolbox", ...
