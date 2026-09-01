42ccf9b5224f8bebab70b7675c86c35cf3cf0a18
Aylin Dmello <admello@mathworks.com>
2026-09-01 16:39:16 -0400

    Add mex stage to default build step. Use cpp/include instead of cpp/vendored.

--- buildfile.m
+++ buildfile.m
@@ -8,7 +8,7 @@ function plan = buildfile
 
     plan = buildplan(localfunctions);
 
-    plan.DefaultTasks = ["lint", "test"];
+    plan.DefaultTasks = ["mex", "lint", "test"];
 
     plan("test").Dependencies = "lint";
     plan("fixLint").Dependencies = "indent";
